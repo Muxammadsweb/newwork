57da21c6218a7649c7e81f816ffa328931ff9712
Muxammad <ravvshanbekov1ch07@gmail.com>
2023-09-12 20:25:19 +0300

    tretiy

---
+++ app.js
@@ -0,0 +1,20 @@
+let dev = document.querySelector(`.dev`);
+let btn1 = document.querySelector(`.btn1`);
+btn1.addEventListener("click", function () {
+  dev.style.display = "none";
+});
+let html = document.querySelector(`.html`);
+let btn2 = document.querySelector(`.btn2`);
+btn2.addEventListener("click", function () {
+  html.style.display = "none";
+});
+let css = document.querySelector(`.css`);
+let btn3 = document.querySelector(`.btn3`);
+btn3.addEventListener("click", function () {
+  css.style.display = "none";
+});
+let JavaScript = document.querySelector(`.JavaScript`);
+let btn4 = document.querySelector(`.btn4`);
+btn4.addEventListener("click", function () {
+  JavaScript.style.display = "none";
+});
